ce9d7233ab8f75e306c293439d61bb364d7315cf
Jack Walton <jack@jumpingrivers.com>
2022-12-15 11:49:40 +0000

    Day 9 solutions

--- day09/part2.py
+++ day09/part2.py
@@ -7,7 +7,7 @@ from utils import load_input, simulate_rope
 
 def main(practice=False):
     head_locs = load_input(practice=practice)
-    locs = simulate_rope(head_locs)
+    locs = simulate_rope(head_locs, rope_length=9)
     locs = np.unique(locs, axis=0).shape[0]
     print(f"The tail visited {locs} unique positions.")
     return locs
--- day09/utils.py
+++ day09/utils.py
@@ -26,3 +26,33 @@ def motions_to_locs(motions):
     )
     head_locs = head_moves.cumsum(0)
     return head_locs
+
+
+def simulate_rope(head_locs, rope_length=1):
+    locs = head_locs.copy()
+    for _ in range(rope_length):
+        locs = follow(locs)
+    return locs
+
+
+def follow(ahead_locs):
+    behind_locs = np.array([[0, 0]])
+    behind_loc = behind_locs[0]
+
+    for ahead_loc in ahead_locs:
+        TH = ahead_loc - behind_loc  # noqa: N806
+        d = np.abs(TH).sum()
+
+        # Don't move
+        if d < 2:
+            continue
+        # If a vertical or horizontal move
+        elif 0 in TH:
+            behind_loc = (ahead_loc + behind_loc) // 2
+        # Diagonal move
+        elif d > 2:
+            behind_loc += np.sign(TH)
+
+        behind_locs = np.vstack([behind_locs, behind_loc])
+
+    return behind_locs
